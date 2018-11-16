2c3ffa40f96a774a643cd43b3828e6ceba5c1cc0
Orlando J Betancourth <orlando.betancourth@gmail.com>
2018-11-16 16:36:29 -0600

    Angular Web Design Finished

--- angularClient/src/app/app-routing.module.ts
+++ angularClient/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent as login} from './login/login.component';
-import {MainComponent as main} from './main/main.component';
+import { LoginComponent as login} from './login/login.component';
+import { MainComponent as main} from './main/main.component';
+import { DetailItemComponent as item} from './detail-item/detail-item.component';
+import { CheckoutComponent as checkout} from './checkout/checkout.component';
 const routes: Routes = [
   {path : 'login', component : login },
-  {path: 'main', component: main }
+  {path: 'main', component: main },
+  {path: 'detail/:id', component: item},
+  {path: 'checkout', component: checkout },
 ];
 
 @NgModule({
--- angularClient/src/app/app.module.ts
+++ angularClient/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { SearchComponent } from './main/search/search.component';
 import { DetailComponent } from './main/detail/detail.component';
-import { NavComponent } from './main/nav/nav.component';
+import { NavComponent } from './nav/nav.component';
+import { DetailItemComponent } from './detail-item/detail-item.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NavComponent } from './main/nav/nav.component';
     MainComponent,
     SearchComponent,
     DetailComponent,
-    NavComponent
+    NavComponent,
+    DetailItemComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
